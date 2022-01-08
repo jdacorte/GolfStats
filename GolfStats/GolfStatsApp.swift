5f60695d19ced4fd4ac4127c0c2fec651f2d2966
John DaCorte <john.dacorte@gmail.com>
2022-01-08 10:25:49 -0500

    Initial Commit

---
+++ GolfStats/GolfStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfStatsApp.swift
+//  GolfStats
+//
+//  Created by John DaCorte on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GolfStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
